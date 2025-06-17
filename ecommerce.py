d8e17b0df8250cce8c306751a051e0d91aa4eb8c
nitinnathgoswami <nitinnathgoswami111@gmail.com>
2025-06-17 16:43:26 +0530

    Delete ecommerce.py
    delete

--- ecommerce.py
+++
@@ -1,6 +0,0 @@
-# Simple E-commerce system in python (CLI)
-# Intern Name:NITIN NATH GOSWAMI
-# Intern id:217256425
-# Internship Task for @indolike
-# Different Emojies picked From @emojiepedia.org
-
