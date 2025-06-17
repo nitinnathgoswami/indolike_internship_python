1eeedad85f8fc28561c479917b91fe339a34c975
nitinnathgoswami <nitinnathgoswami111@gmail.com>
2025-06-17 16:34:00 +0530

    Add files via upload

---
+++ ecommerce.py
@@ -0,0 +1,6 @@
+# Simple E-commerce system in python (CLI)
+# Intern Name:NITIN NATH GOSWAMI
+# Intern id:217256425
+# Internship Task for @indolike
+# Different Emojies picked From @emojiepedia.org
+
